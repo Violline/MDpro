e7220d4668ff2e159be43c52343a66e5595c6602
Violline <ola.mankiewicz@gmail.com>
2016-05-26 19:13:09 +0200

    Create videoread.cpp

---
+++ videoread.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+Mat OurImage, camera, gray, edges, morph;
+
+
+
+int main(int, char**)
+{
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
+
+	Mat edges;
+	namedWindow("edges", 1);
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+		cvtColor(frame, edges, COLOR_BGR2GRAY);
+		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		Canny(edges, edges, 0, 30, 3);
+		imshow("edges", edges);
+		if (waitKey(30) >= 0) break;
+	}
+	// the camera will be deinitialized automatically in VideoCapture destructor
+	return 0;
+}
